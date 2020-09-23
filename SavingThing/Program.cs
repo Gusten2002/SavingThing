c4a1132d2e42928c9b8fc00aa563bd80ad524d6a
August <august.agren@elev.ga.ntig.se>
2020-09-23 10:09:39 +0200

    first

---
+++ SavingThing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SavingThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
