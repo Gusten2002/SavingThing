9d18c819477c3f7cd2c87894983fbdeaf75ee47d
August <august.agren@elev.ga.ntig.se>
2020-09-23 11:10:30 +0200

    mostly done

--- SavingThing/Program.cs
+++ SavingThing/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace SavingThing
 {
@@ -6,7 +9,29 @@ namespace SavingThing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string [] ans = ("1", "2");
+            XmlSerializer cookieSerializer = new XmlSerializer(typeof(cookie));
+
+            string input = Console.ReadLine();
+
+            while (!ans.Contains(input))
+            {
+                Console.WriteLine("Do you have 1 or 2 cookies?");
+                input = Console.ReadLine();
+            }
+  
+
+            cookie c = new cookie();
+            
+            FileStream file = File.Open(@"cookies.xml", FileMode.OpenOrCreate);
+
+            cookieSerializer.Serialize(file, c);
+
+            file.Close();
+            
+            // Console.WriteLine(c.chocolate);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ SavingThing/cookie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SavingThing
+{
+    public class cookie
+    {
+        public float chocolate = 2.5f;
+        public int wheat = 5;
+        public int ingredients = 2;
+    }
+}
